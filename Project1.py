53fe8bf2b514c8cb4cf5f1cb285701eb5738aba3
Jeremi Swann <1616369+jeremii@users.noreply.github.com>
2019-01-20 07:13:05 -0500

    Project 1 complete

--- Project1.py
+++ Project1.py
@@ -3,6 +3,9 @@
 # takes in number of declarative statements.
 # and ouputs a truth table.
 def lex(n):
+    # n = count of statements
+    # r = all possible combinations of T&F**n power
+    # Vertical height of table
     # r equals 2(T&F) to the power of statement count
     r=2**n
     # divider line
@@ -10,15 +13,14 @@ def lex(n):
     # Vertical print out
     # For every increment in r, do this
     for i in range(r):
-        a="|"
+        a = "|"
         # Print divider line
         print(line)
         # Horizontal print out
         # For every increment in n, do this
         for j in range(n):
-            # TRANSLATE THIS TO ENGLISH
-            # if r divided by 2 to the increment+1's power
-            # divided by i modulus 2 is a even number, append TRUE 
+            # if row total divided by 2 to the increment's power
+            # divided by row increment is even, append TRUE 
             if(i//(r/2**(j+1)))%2==0:
                 a=a+" T |"
                 continue
@@ -26,5 +28,6 @@ def lex(n):
             a = a+" F |"
         # PRINT a for every increment in r
         print(a)
+    print(line)
 
 lex(3)
