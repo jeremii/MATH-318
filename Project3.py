9652211b6edd9c0f18e942b82d720f3d3228b80a
Jeremi Swann <1616369+jeremii@users.noreply.github.com>
2019-02-07 14:15:55 -0500

    p3 complete

--- Project3.py
+++ Project3.py
@@ -1,14 +1,17 @@
 import Project2
 
-def run(a): 
-    for p in range(True, False):
-        for q in range(True, False):
-            print( p + " " + q + " " )
-            print ( (p == q) + " " )
-            print( (p != q) + " " )
-            print( (not p) + " " + (not q) + " " )
-            print( (not p or not q) + " " )
-            print( Project2.iff(p, q) )
-
-
-run(True)
+def run(): 
+    print( "P \t Q \t P^Q \t ~(P^Q)  ~P \t ~Q \t ~Pv~Q \t iff \t implies" )
+    for p in (True, False):
+        line = ""
+        for q in (True, False):
+            line += str(p) + " \t " + str(q) + " \t "
+            line += str(p == q) + " \t "
+            line += str(p != q) + " \t "
+            line += str(not p) + " \t " + str(not q) + " \t "
+            line += str(not p or not q) + " \t " 
+            line += str(Project2.iff(not p, not q)) + " \t "
+            line += str(Project2.implies( not p, not q))
+            print line
+            line = ""
+run()
