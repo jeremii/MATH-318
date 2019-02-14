46389851ae59f58d0b3d2e18ab20749e89f2a40a
mst0ffel <michael@stoffel.tech>
2019-02-13 22:35:55 -0500

    Project 5 complete

--- Project 5/Project 5.py
+++ Project 5/Project 5.py
@@ -25,3 +25,55 @@ print(sorted(A))
 # Sort set B into ascending order
 print("List B sorted into ascending order")
 print(sorted(B))
+
+# Union of A and B
+
+#Print label for Union of A and B
+print("Union of A and B")
+#Define function for union of A and B
+def Union(A, B):
+    # Set variable for union of A and B to list A + list B and then sort them into ascending order
+    unionAB = sorted(A + B)
+    # Remove Duplicate values from list that is Union of A and B
+    removeDupUnionAB = list( dict.fromkeys(unionAB))
+    return removeDupUnionAB
+#Print Union of A and B
+print(Union(A, B))
+
+# Print List A sorted into ascending order again
+# Label List A
+print("List A sorted into ascending order")
+print(sorted(A))
+# Print List B sorted into ascending order again
+# Label List B
+print("List B sorted into ascending order")
+print(sorted(B))
+
+#Print not A in ascending order
+#Print label for not A in ascending order
+print("Not A in ascending order")
+
+# Make a list of all possible numbers from 0 - 200
+allNumbers = list(range(201))
+# Define function to find the difference of list A and the list of all possible numbers from 0-200 (allNumbers - A)
+def missingFromA(allNumbers, A):
+    # Set variable for difference of allNumbers and A to list allNumbers - A and then sort them into ascending order
+    # Find the difference between list allNumbers and list A
+    notInA = [i for i in allNumbers + A if i not in allNumbers or i not in A]
+    return sorted(notInA)
+
+notA = missingFromA(allNumbers, A)
+
+print(notA)
+
+#Print A-B in ascending order
+#Print label for A-B in ascending order
+print("A-B in ascending order")
+#Define function for A-B
+def diff(A, B):
+    # Set variable for difference of A and B to list A - list B and then sort them into ascending order
+    #Find the difference between list A and list B
+    diffAB = [i for i in A + B if i not in A or i not in B]
+    return sorted(diffAB)
+
+print(diff(A, B))
