9968066c70a68fdae5a21129ca658104920da45e
Jeremi Swann <1616369+jeremii@users.noreply.github.com>
2019-03-20 04:25:50 -0400

    Insertion sort fix

--- Project 6/Project6.py
+++ Project 6/Project6.py
@@ -3,22 +3,16 @@ import Project5 as p5
 
 array = p5.irand(100, 200)
 
+
+
 def insertionSort(arr): 
-  
-    # For every element in the array, do this:
-    for i in range(1, len(arr)): 
-        # assign key as the array item in this current iteration
-        key = arr[i] 
-  
-        # j will be one iteration back
+    for i in range(1, len(arr)):
+        val = arr[i]
         j = i-1
-        # while j is >= 0 and the arr[i] is less than arr[j]
-        while j >=0 and key < arr[j] : 
-            #insertion sort
-            arr[j+1] = arr[j] 
-            # j decrements by 1
-            j -= 1
-        arr[j+1] = key 
+        while( j>= 0) and (arr[j]>val):
+            arr[j+1] = arr[j]
+            j = j-1
+        arr[j+1] = val
     return arr
 
 array = insertionSort(array)
