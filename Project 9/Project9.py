5f2dbd38efad715549cb4f9d3bff155f38a09604
Jeremi Swann <1616369+jeremii@users.noreply.github.com>
2019-04-16 09:48:42 -0400

    Update Project9.py

--- Project 9/Project9.py
+++ Project 9/Project9.py
@@ -1,6 +1,18 @@
 one = int(input("Enter a number: "))
 two = int(input("Enter a second number: "))
 
+def gcdandlcm(a):
+    n = len(a)
+    gcd = euc(a[0], a[1])
+    lcm = a[0]*a[1]/gcd
+    #print(lcm)
+    for i in range( 2, n ):
+        gcd = euc( gcd, a[i])
+        gcdlcm = euc(lcm, a[i])
+        #print gcdlcm
+        lcm = lcm*a[i]/gcdlcm
+        #print lcm
+    return gcd, lcm
 
 
 def lcm(a, b):
@@ -36,4 +48,8 @@ def test(arr):
                 break
             print( "Greatest Common Divisor of "+str(a) + " & "+ str(b)+ " = " + str(euc(a,b)) )
             print( "Least Common Multiple of "+str(a) + " & "+ str(b)+ " = " + str(lcm(a,b)) + "\n")
-test(array)
+
+print( "The GCD and LCM of the array "+ str(array) +" is ")
+answer = gcdandlcm(array)
+print ( "GCD: " + str(answer[0]) )
+print ( "LCM: " + str(answer[1]) )
